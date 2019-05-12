5f9e5beec5b24f47c247a2dc33899098ae203724
BakhtiyarShamilzada <46760730+BakhtiyarShamilzada@users.noreply.github.com>
2019-05-13 00:55:28 +0400

    Add files via upload

---
+++ 8-May-2019/8-May-2019/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_May_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+
+            //Console.WriteLine("Eded daxil edin:");
+            //var input = int.Parse(Console.ReadLine());
+            //int cem = 0;
+            //for (int i = 1; i <= input; i++)
+            //{
+            //    cem += i;
+            //}
+            //Console.WriteLine("ededlerin cemi: " + cem);
+
+            // Task 2
+
+            //Console.WriteLine("Eded daxil edin:");
+            //var input = int.Parse(Console.ReadLine());
+            //int cem = 0;
+            //for (int i = 1; i <= input; i++)
+            //{
+            //    if(i % 2 == 0)
+            //    cem += i;
+            //}
+            //Console.WriteLine("Cut ededlerin cemi: " + cem);
+
+            // Task 3
+
+            //Console.WriteLine("Eded daxil edin:");
+            //var input = Console.ReadLine();
+            //var cem = 0;
+            //char[] numbers = input.ToCharArray();
+            //foreach (char number in numbers)
+            //{
+            //    cem += Convert.ToInt32(number.ToString());
+            //}
+            //Console.WriteLine("Reqemleri Cemi: " + cem);
+
+            // Task 4
+
+            //Console.WriteLine("Eded daxil edin:");
+            //var input = Console.ReadLine();
+            //int index = input.IndexOf("@");
+            //if(index != -1)
+            //{
+            //    Console.WriteLine("Düzgün email daxil etmisiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yanlış email daxil etmisiniz");
+            //}
+        }
+    }
+}
